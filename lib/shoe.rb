4570db6498a2949b3a56843dba29bbd58c95de31
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-15 21:35:09 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+BRANDS = []
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+  else
+    BRANDS << brand
+  end
   end
 
   def cobble
@@ -12,3 +16,22 @@ class Shoe
   end
 
 end
+
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title, :genre 
+
+# GENRES = []
+#   def initialize(title)
+#     @title = title
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+#   def genre=(genre)
+#     @genre = genre
+#     GENRES << genre 
+#   end
+
+# end
